f950179d6eff149b353e21adba55cdda3ab5cdf9
ZoOL <zhooul@gmail.com>
2023-08-07 19:47:11 +0800

    update example

--- monitor/src/plugin.rs
+++ monitor/src/plugin.rs
@@ -1,16 +1,21 @@
+use crate::systems::world_entities_count;
 use bevy::app::{App, Plugin, Update};
-use bevy::diagnostic::{Diagnostic, RegisterDiagnostic};
-use crate::systems::{world_entities_count, WORLD_ENTITIES_COUNT};
+use bevy::diagnostic::{Diagnostic, DiagnosticId, RegisterDiagnostic};
 
 pub struct EcsMonitorPlugin;
 
 impl Plugin for EcsMonitorPlugin {
     fn build(&self, app: &mut App) {
-        app
-            .register_diagnostic(
-                Diagnostic::new(WORLD_ENTITIES_COUNT, "world_ecs_count", 10)
-                    .with_suffix(" count"),
-            )
-            .add_systems(Update, world_entities_count);
+        app.register_diagnostic(Diagnostic::new(
+            Self::WORLD_ENTITIES_COUNT,
+            "world_ecs_count",
+            10,
+        ))
+        .add_systems(Update, world_entities_count);
     }
+}
+
+impl EcsMonitorPlugin {
+    pub const WORLD_ENTITIES_COUNT: DiagnosticId =
+        DiagnosticId::from_u128(117259298154673752838376838005950978623);
 }
--- rest/examples/basic.rs
+++ rest/examples/basic.rs
@@ -3,7 +3,23 @@ use bevy::prelude::*;
 
 use bevy_ecs_explorer::EcsRestPlugin;
 
+#[derive(Component)]
+pub struct Ball {
+    velocity: Vec3,
+}
+
 fn main() {
-    App::new().add_plugins(DefaultPlugins)
-        .add_plugins((LogDiagnosticsPlugin::default() , EcsRestPlugin)).run();
+    App::new()
+        .add_plugins(DefaultPlugins)
+        .add_plugins((LogDiagnosticsPlugin::default(), EcsRestPlugin))
+        .add_systems(Update, mouse_handler)
+        .run();
+}
+
+fn mouse_handler(mut commands: Commands, mouse_button_input: Res<Input<MouseButton>>) {
+    if mouse_button_input.pressed(MouseButton::Left) {
+        commands.spawn(Ball {
+            velocity: Vec3::new(0.0, 0.0, 0.0),
+        });
+    }
 }
